c473fea3fa24662b6f2ea53c81135f96b25ea1c2
Tarnnsoft <tarnnsoft@gmail.com>
2020-10-28 11:39:11 +0530

    added standard for linting

--- src/index.js
+++ src/index.js
@@ -1,27 +1,27 @@
 import { useReducer } from 'react'
 
-export default useEasyState = initialValue => {
+const useEasyState = initialValue => {
   const [state, dispatch] = useReducer(
-      (state, { key, value }) => ({
-        ...state,
-        [key]: value
-      }),
-      initialValue
-    ),
-    get = key => state[key],
-    set = key => value =>
-      dispatch({
-        type: 'SET',
-        key,
-        value
-      }),
-    helpers = Object.keys(initialValue).reduce((acc, curr) => {
-      const setKey = `set${curr.charAt(0).toUpperCase()}${curr.substr(1)}`
-      return {
-        ...acc,
-        [setKey]: set(curr)
-      }
-    }, {})
+    (state, { key, value }) => ({
+      ...state,
+      [key]: value
+    }),
+    initialValue
+  )
+  const get = key => state[key]
+  const set = key => value =>
+    dispatch({
+      type: 'SET',
+      key,
+      value
+    })
+  const helpers = Object.keys(initialValue).reduce((acc, curr) => {
+    const setKey = `set${curr.charAt(0).toUpperCase()}${curr.substr(1)}`
+    return {
+      ...acc,
+      [setKey]: set(curr)
+    }
+  }, {})
 
   return {
     get,
@@ -30,3 +30,5 @@ export default useEasyState = initialValue => {
     ...helpers
   }
 }
+
+export default useEasyState
